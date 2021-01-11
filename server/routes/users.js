b5b8c556f531b3f603dd21f5f80c949cc934eb4c
Calypso Asline <aslinecalypso@gmail.com>
2021-01-11 21:54:06 +0100

    -musers route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,26 @@
 const express = require("express");
+const { param } = require(".");
+const User = require("../models/User");
 const router = express.Router();
 
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+router.get("/", async function (req, res, next) {
+  try {
+    const allUsers = await User.find();
+    res.json(allUsers);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.patch("/me", async function (req, res, next) {
+  try {
+    const paramUser = req.session.currentUser;
+    const userContent = req.body;
+    const updateUser = await User.findByIdAndUpdate(paramUser, userContent);
+    res.json(updateUser);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
